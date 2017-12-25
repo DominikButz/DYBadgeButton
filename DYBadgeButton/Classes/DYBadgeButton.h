8df24da63773b5d3dfa9a09b7c00d2bdcf91f3ba
Dominik <dominikbutz@gmail.com>
2017-12-25 18:00:07 +0100

    Adding header file

---
+++ DYBadgeButton/Classes/DYBadgeButton.h
@@ -0,0 +1,21 @@
+//
+//  DYBadgeButton.h
+//  DYBadgeButton
+//
+//  Created by Dominik Butz on 25.12.17.
+//
+#import <UIKit/UIKit.h>
+
+#ifndef DYBadgeButton_h
+#define DYBadgeButton_h
+
+//! Project version number for DYCornerBanner.
+FOUNDATION_EXPORT double DYBadgeButtonVersionNumber;
+
+//! Project version string for DYCornerBanner.
+FOUNDATION_EXPORT const unsigned char DYBadgeButtonVersionString[];
+
+#endif /* DYBadgeButton_h */
+
+
+
