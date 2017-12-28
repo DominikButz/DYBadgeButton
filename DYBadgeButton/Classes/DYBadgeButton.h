6c5e3955e8b6d6c58705ba601a7cbb291cc67f82
Dominik <dominikbutz@gmail.com>
2017-12-28 17:20:53 +0100

    build phase corrections

--- DYBadgeButton/Classes/DYBadgeButton.h
+++ DYBadgeButton/Classes/DYBadgeButton.h
@@ -9,10 +9,10 @@
 #ifndef DYBadgeButton_h
 #define DYBadgeButton_h
 
-//! Project version number for DYCornerBanner.
+//! Project version number
 FOUNDATION_EXPORT double DYBadgeButtonVersionNumber;
 
-//! Project version string for DYCornerBanner.
+//! Project version string
 FOUNDATION_EXPORT const unsigned char DYBadgeButtonVersionString[];
 
 #endif /* DYBadgeButton_h */
